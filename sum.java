a222e460c1823c69191cc44e323bc0b6296cefe9
GOPINATH <ngopi6769@gmail.com>
2024-12-16 09:07:08 +0530

    commit

--- sum.java
+++ sum.java
@@ -16,7 +16,14 @@ public class sum {
         }
 
         System.out.println("original array is : ");
-        
+
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+
+        System.out.println("sum of the array : "+sum);
+
     }
     
 }
