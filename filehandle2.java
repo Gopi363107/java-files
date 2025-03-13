16382e0a0ba9f8ea9be41f37f38a8530ebf94e24
GOPINATH <ngopi6769@gmail.com>
2025-03-13 22:29:35 +0530

    commit

--- filehandle2.java
+++ filehandle2.java
@@ -1,3 +1,29 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.IOException;
+
+
 public class filehandle2 {
+    public static void main(String args[]){
+        try{
+           File javafiles = new File ("filehandle.java");//javafiles--is a folder name
+           if(javafiles.createNewFile()){
+                System.out.println("file created :"+javafiles.getName());
+           } else{
+                System.out.println("file already exists.");
+           } 
+        }
+        catch (IOException e) {
+            System.out.println("file is not created .");
+            e.printStackTrace();
+        }
+        
+
+    }
+
+    
+    
+    
+
     
 }
